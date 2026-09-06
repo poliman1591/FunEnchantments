d907b6537a4dd2443468d726bc2481a70b513e50
poliman1591 <qaz661548@gmail.com>
2026-09-06 22:35:44 +0300

    Update FunEnchantments.java

--- src/main/java/ru/poliman1591/funenchantments/FunEnchantments.java
+++ src/main/java/ru/poliman1591/funenchantments/FunEnchantments.java
@@ -4,13 +4,27 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FunEnchantments extends JavaPlugin {
 
+    private EnchantmentManager enchantmentManager;
+
     @Override
     public void onEnable() {
+
+        enchantmentManager = new EnchantmentManager(this);
+
+        getServer().getPluginManager()
+                .registerEvents(enchantmentManager, this);
+
+        enchantmentManager.startMagnetTask();
+
         getLogger().info("FunEnchantments включён!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("FunEnchantments выключен!");
+    }
+
+    public EnchantmentManager getEnchantmentManager() {
+        return enchantmentManager;
     }
 }
